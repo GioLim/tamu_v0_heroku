0fbd9a4e576679bc386db718bac5882fd1fee9b1
Ubuntu <ubuntu@ip-172-31-27-63.us-east-2.compute.internal>
2019-01-03 06:53:42 +0000

    add pages

---
+++ app/controllers/advancedsearch_controller.rb
@@ -0,0 +1,4 @@
+class AdvancedsearchController < ApplicationController
+  def new
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'app/views/login/index.html.erb', to: 'login#index', as: 'login'
+  get 'app/views/welcome/index.html.erb', to: 'welcome#index', as: 'welcome'
+  get 'app/views/advancedsearch/index.html.erb', to: 'advancedsearch#index', as: 'advancedsearch'
+
+  resources :login
+  resources :advancedsearch
+
+  root 'welcome#index'
+end
